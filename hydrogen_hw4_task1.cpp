dbeb07722a6c7a2032dce212d9f2c1d3db203bf0
Carson Tyler <carsontyler@mail.weber.edu>
2017-10-10 12:55:55 -0600

    Initial commit

---
+++ hydrogen_hw4_task1.cpp
@@ -0,0 +1,29 @@
+// File: hydrogen_hw4_task1
+// Created by Carson Tyler on 10/10/2017 for CS1410.
+// Copyright ©2017 WSU
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Constants, Structs, Classes
+class
+{
+private:
+
+public:
+    //Constructor
+
+//Prototypes
+    
+};
+
+//Prototypes
+
+// Main Program
+int main()
+{
+    
+    
+    return 0;
+}
+// Function Definitions
