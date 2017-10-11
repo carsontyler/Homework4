6232ea2b3a580fa8c22f124610b3853d24ff0cb1
Carson Tyler <carsontyler@mail.weber.edu>
2017-10-11 12:07:09 -0600

    Initial Finish. Works.

--- hydrogen_hw4_task1.cpp
+++ hydrogen_hw4_task1.cpp
@@ -4,9 +4,10 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 // Constants, Structs, Classes
 const int SIZE = 3;
-const int INT_MAX = 1;
+const int MAX = 1;
 
 class HouseInfo
 {
@@ -18,31 +19,102 @@ public:
     //Constructor
     HouseInfo(): streetNum(0), price(0.0){}
 
-    void HouseInput ()
+    void HouseInput()
     {
         // Ask user for street number
-        cout << "Please enter the street number ";
+        cout << "\nPlease enter the street number: ";
         cin >> streetNum;
         // Ask user for street name
-        cout << "Please enter the street name ";
+        cout << "Please enter the street name: ";
         cin >> streetName;
-        cin.ignore(INT_MAX);
+        cin.ignore(MAX);
         // Ask user for house price
-        cout << "Please enter the price ";
-        cin >> streetName;
+        cout << "Please enter the price: ";
+        cin >> price;
         cout << "" << endl;
     }
+    void HouseOutput()
+    {
+        cout << streetNum << " " << streetName << " for $["<< price << "]" << endl;
+    }
+    double DisplayPrice()
+    {
+       return price;
+    }
 };
+
 //Prototypes
+void comparePrices(HouseInfo arr[]);
 
 // Main Program
 int main()
 {
-    HouseInfo h1, h2, h3;                               // Declare house variables
+    HouseInfo h1, h2, h3;
     HouseInfo array[SIZE];                              // Declare array
+    cout << "Enter info for the first house ";
     h1.HouseInput();
-    h1 = array[0];
+    array[0] = h1;
+    cout << "Enter info for the second house ";
+    h2.HouseInput();
+    array[1] = h2;
+    cout << "Enter info for the third house ";
+    h3.HouseInput();
+    array[2] = h3;
+
+    cout << "Your Market analysis is as follows: " << endl;
+    cout << "1 house at "; array[0].HouseOutput();
+    cout << "2 house at "; array[1].HouseOutput();
+    cout << "3 house at "; array[2].HouseOutput();
+    cout << endl;
+
+    comparePrices(array);
 
     return 0;
 }
+
 // Function Definitions
+void comparePrices(HouseInfo arr[])
+{
+    if (arr[0].DisplayPrice() < arr[1].DisplayPrice())
+    {
+        if (arr[0].DisplayPrice() < arr[2].DisplayPrice())
+        {
+            cout << "You should buy: House Number 1" << endl;
+        }
+        if (arr[0].DisplayPrice() > arr[2].DisplayPrice())
+        {
+            cout << "You should buy: House Number 3" << endl;
+        }
+        if (arr[0].DisplayPrice() == arr[2].DisplayPrice())
+        {
+            cout << "You should buy: House Number 1\nHouse Number 3" << endl;
+        }
+    }
+    else if (arr[1].DisplayPrice() < arr[0].DisplayPrice())
+    {
+        if (arr[1].DisplayPrice() < arr[2].DisplayPrice())
+        {
+            cout << "You should buy: House Number 2" << endl;
+        }
+        if (arr[2].DisplayPrice() < arr[1].DisplayPrice())
+        {
+            cout << "You should buy: House Number 3" << endl;
+        }
+        if (arr[1].DisplayPrice() == arr[2].DisplayPrice())
+        {
+            cout << "You should buy: House Number 2\nHouse Number 3" << endl;
+        }
+    }
+    else if (arr[0].DisplayPrice() == arr[1].DisplayPrice())
+    {
+        cout << "You should buy: House Number 1\nHouse Number 2" << endl;
+        if (arr[0].DisplayPrice() == arr[2].DisplayPrice())
+        {
+            cout << "House Number 3" << endl;
+        }
+    }
+    else
+    {
+        cout << "Internal error, please try again" << endl;
+    }
+}
